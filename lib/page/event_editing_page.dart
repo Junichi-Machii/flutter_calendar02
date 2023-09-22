efa543ce8ed14cff8c28e3afbaef20339292b5e0
Junichi-Machii <junichi878338@hotmail.co.jp>
2023-09-22 12:16:12 +0900

    add buildDateTimePicker()

---
+++ lib/page/event_editing_page.dart
@@ -0,0 +1,207 @@
+import 'dart:html';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_cakendar_app02/utils.dart';
+import 'package:provider/provider.dart';
+
+class EventEditingPage extends StatefulWidget {
+  EventEditingPage({super.key, this.event});
+
+  final Event? event;
+
+  @override
+  State<EventEditingPage> createState() => _EventEditingPageState();
+}
+
+class _EventEditingPageState extends State<EventEditingPage> {
+  final _formKey = GlobalKey<FormState>();
+  final titleController = TextEditingController();
+  late DateTime fromDate;
+  late DateTime toDate;
+
+  @override
+  void initState() {
+    if (widget.event == null) {
+      fromDate = DateTime.now();
+      toDate = DateTime.now().add(Duration(hours: 2));
+    }
+  }
+
+  @override
+  void dispose() {
+    titleController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: CloseButton(),
+        actions: buildEditingActions(),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(12),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              buildTitle(),
+              SizedBox(height: 12),
+              buildDateTimePicker(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> buildEditingActions() => [
+        ElevatedButton.icon(
+            style: ElevatedButton.styleFrom(
+              primary: Colors.transparent,
+              shadowColor: Colors.transparent,
+            ),
+            onPressed: () {},
+            icon: Icon(Icons.done),
+            label: Text('SAVE'))
+      ];
+
+  Widget buildTitle() => TextFormField(
+        style: TextStyle(fontSize: 24),
+        decoration: InputDecoration(
+          border: UnderlineInputBorder(),
+          hintText: 'Add Title',
+        ),
+        onFieldSubmitted: (_) {},
+        validator: (title) =>
+            title != null && title.isEmpty ? 'Title cannot be empty' : null,
+        controller: titleController,
+      );
+
+  Widget buildDateTimePicker() => Column(
+        children: [
+          buildForm(),
+          buildTo(),
+        ],
+      );
+
+  Widget buildForm() => buildHeader(
+        header: 'FROM',
+        child: Row(
+          children: [
+            Expanded(
+              flex: 2,
+              child: buildDropdownField(
+                text: Utils.toDate(fromDate),
+                onClicked: () => pickFromDateTime(pickDate: true),
+              ),
+            ),
+            Expanded(
+              child: buildDropdownField(
+                text: Utils.toTime(fromDate),
+                onClicked: () => pickFromDateTime(pickDate: false),
+              ),
+            ),
+          ],
+        ),
+      );
+
+  Widget buildTo() => buildHeader(
+        header: 'TO',
+        child: Row(
+          children: [
+            Expanded(
+              flex: 2,
+              child: buildDropdownField(
+                text: Utils.toDate(toDate),
+                onClicked: () => pickToDateTime(pickDate: true),
+              ),
+            ),
+            Expanded(
+              child: buildDropdownField(
+                text: Utils.toTime(toDate),
+                onClicked: () => pickToDateTime(pickDate: false),
+              ),
+            ),
+          ],
+        ),
+      );
+
+  Widget buildDropdownField({
+    required String text,
+    required dynamic onClicked,
+  }) =>
+      ListTile(
+        title: Text(text),
+        trailing: Icon(Icons.arrow_drop_down),
+        onTap: onClicked,
+      );
+
+  Widget buildHeader({
+    required String header,
+    required Widget child,
+  }) =>
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            header,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          child
+        ],
+      );
+
+  Future pickFromDateTime({required bool pickDate}) async {
+    final date = await pickDateTime(fromDate, pickDate: pickDate);
+    if (date == null) return;
+
+    setState(() => fromDate = date);
+  }
+
+  Future pickToDateTime({required bool pickDate}) async {
+    final date = await pickDateTime(
+      toDate,
+      pickDate: pickDate,
+      firstDate: pickDate ? fromDate : null, // From Date より前が選択出来ないようにする
+    );
+    if (date == null) return;
+
+    setState(() => toDate = date);
+  }
+
+  Future<DateTime?> pickDateTime(
+    DateTime initialDate, {
+    required bool pickDate,
+    DateTime? firstDate,
+  }) async {
+    if (pickDate) {
+      final date = await showDatePicker(
+        context: context,
+        initialDate: initialDate,
+        firstDate: firstDate ?? DateTime(2020, 1),
+        lastDate: DateTime(2101),
+      );
+
+      if (date == null) return null;
+
+      final time =
+          Duration(hours: initialDate.hour, minutes: initialDate.minute);
+      return date.add(time);
+    } else {
+      final timeOfDay = await showTimePicker(
+        context: context,
+        initialTime: TimeOfDay.fromDateTime(initialDate),
+      );
+
+      if (timeOfDay == null) return null;
+      final date =
+          DateTime(initialDate.year, initialDate.month, initialDate.day);
+      final time = Duration(hours: timeOfDay.hour, minutes: timeOfDay.minute);
+      return date.add(time);
+    }
+  }
+}
